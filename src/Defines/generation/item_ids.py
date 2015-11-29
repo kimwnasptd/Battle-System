2ba3fdbebd4a023a60ae58235aa94b0d9a0d5e89
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-11-28 20:46:32 -0500

    added item headers
    Header file for items created and added

---
+++ src/Defines/generation/item_ids.py
@@ -0,0 +1,20 @@
+import re
+with open('C:/Users/Dilshan/Desktop/Battle-System/src/Defines/generation/items') as f:
+     lines = f.read().splitlines()
+
+with open('C:/Users/Dilshan/Desktop/Battle-System/src/Defines/items.h', "w") as f:
+     # header guards     
+     f.write("#ifndef ITEM_IDS" + "\n" + "#define ITEM_IDS" + "\n"*3)  
+     for line in lines:
+          hex_val = hex(int(line.split("\t")[0]))
+          new_str = ""
+          for char in hex_val:
+               if (char != "x"):
+                    new_str += char.capitalize()
+               else:
+                    new_str += char
+          f.write("#define " + (line.split("\t")[2]).upper().replace(" ", "_").replace("-", "_")
+                  + " " + new_str + "\n")
+
+     #header guards end
+     f.write("\n" + "#endif /* ITEM_IDS */" + "\n")
